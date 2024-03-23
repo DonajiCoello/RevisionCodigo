6de24eef8266047789a0656886829787fd4b5100
Donaji Coello <donnsdonns14@gmail.com>
2024-03-22 20:08:17 -0600

    Codigo corregido primera revision

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $b = document.querySelector('#blog'); /* Esta mal la referencia, es sin # */
+const $l = document.querySelector('.location'); /* No esta referenciada en el documento html */
 
-function displayUser(username) {
+/* Aqui le falta una funcion asincrona, await no esta dentro de una función asincrona */
+
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; /* Segun yo aquí, van estas comillas `` para concatenar, no las simples */
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err};` /* Esta mal el nombre de la variable, arriba esta declarada con $ */
 }
 
 displayUser('stolinski').catch(handleError);
